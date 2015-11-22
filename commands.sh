cf05c58fbb63e7a2d544ac1c64c801620c50d638
Ankit Srivastava <ankitsrivastava108@gmail.com>
2015-11-23 00:29:59 +0530

    dirent

---
+++ commands.sh
@@ -0,0 +1,3 @@
+sudo -i service portmap stop
+sudo -i rpcbind -i -w
+sudo -i service portmap start
---
+++ compile.sh
@@ -0,0 +1,2 @@
+gcc operations_client.c operations_clnt.c operations_xdr.c -o client
+gcc operations_svc.c operations_server.c operations_xdr.c -o server
